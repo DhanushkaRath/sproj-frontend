d999ec4be6ec23a511ab6e05db6db17c45e21e17
DhanushkaRath <dhanushka.rathsl@gmail.com>
2025-04-04 22:30:17 +0530

    Remove all files from the repository

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-
-export const initializeMiddleware = (app: express.Application) => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-}; 
--- src/pages/sign-up.page.jsx
+++
@@ -1,8 +0,0 @@
-import { SignUp } from "@clerk/clerk-react";
-
-export default function SignUpPage() {
-  0
-  return (<main className="flex items-center justify-center px-4 min-h-screen">
-    <SignUp />
-  </main>)
-}
