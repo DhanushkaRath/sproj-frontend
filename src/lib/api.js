1fb2f106eec3a04ad5f0a24430fd27fbc4b7c962
DhanushkaRath <dhanushka.rathsl@gmail.com>
2025-04-05 02:28:56 +0530

    add backend url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "https://fed-storefront-backend-manupa.onrender.com/api/";
+const BACKEND_URL = "https://fed-storefront-backend-dhanushka.onrender.com/api/";
 
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
